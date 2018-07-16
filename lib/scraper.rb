f89ce4b0c3cce6e2649689234798a07dfd83e037
Will Lee <will.i.lee@outlook.com>
2018-07-16 19:01:12 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,7 @@ require 'open-uri'
 require_relative './course.rb'
 
 class Scraper
-  
+
 
   def print_courses
   self.make_courses
@@ -18,11 +18,9 @@ class Scraper
   end
 
   def get_page
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
   end
 
-  
-  
+
+
 end
-
-
-
